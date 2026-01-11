727a2635881e13232d9600ffbdffcdd9b3d21139
Michael Gunderson <michael@portablesheep.com>
2026-01-10 22:13:45 -0600

    Fix for failing unit tests in GH actions

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -4,17 +4,17 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$'
+    }
+  },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@angular)'],
   moduleNameMapper: {
     '^@wailsjs/(.*)$': '<rootDir>/wailsjs/$1'
   },
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$'
-      }
-    ]
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
   }
 };
